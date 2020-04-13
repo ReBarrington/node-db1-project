0e5752b880d8348920dcea04bdfef3e9c9abed45
Reagan Barrington <reagan@barringtonfamily.net>
2020-04-13 17:44:40 -0400

    MVP

--- data/accounts/accountsRouter.js
+++ data/accounts/accountsRouter.js
@@ -20,7 +20,6 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
     db('accounts')
         .where({ id: req.params.id })
-        .first()
         .then(account => {
             res.status(200).json({ data: account })
         })
@@ -49,5 +48,34 @@ router.post('/', (req, res) => {
             res.status(500).json({ error: error.message })
         });
 });
+
+router.patch('/:id', (req, res) => {
+    const changes = req.body;
+    // update accounts... set account = 'new account' where id = 
+    db('accounts')
+        .where({ id: req.params.id }) // filtering
+        .update(changes)
+        .then(count => {
+            if (count > 0) {
+                res.status(200).json({ message: "update successful."})
+            } else {
+                res.status(404).json({ message: " no accounts by that id found. "})
+            }
+        })
+});
+
+router.delete('/:id', (req, res) => {
+    db('accounts')
+        .where({ id: req.params.id })
+        .del()
+        .then(result => {
+            console.log(result, " is result.")
+            res.status(200).json({ message: "account deleted."})
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(404).json({ message: "account not found." })
+        })
+})
 
 module.exports = router;
